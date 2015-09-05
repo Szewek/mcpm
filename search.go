7fb6baaef00f5940dcd35ae8059eb6eaf92b8956
Szewek <shevkpl@gmail.com>
2015-09-05 18:36:42 +0200

    Remove duplicates during search

--- search.go
+++ search.go
@@ -5,11 +5,35 @@ import (
 	"regexp"
 )
 
+type (
+	IDArray []int
+)
+
 var pkgTypeName = map[_PackageType]string{
 	type_Mod:          "Mod",
 	type_ModPack:      "Modpack",
 	type_ResourcePack: "Resource pack",
 	type_WorldSave:    "World save",
+}
+
+func (ia *IDArray) Size() int {
+	return len(*ia)
+}
+func (ia *IDArray) Get(idx int) int {
+	return (*ia)[idx]
+}
+func (ia *IDArray) Contains(i int) bool {
+	for j := 0; j < len(*ia); j++ {
+		if i == (*ia)[j] {
+			return true
+		}
+	}
+	return false
+}
+func (ia *IDArray) Add(i int) {
+	if !ia.Contains(i) {
+		(*ia) = append(*ia, i)
+	}
 }
 
 func searchPackage() {
@@ -20,25 +44,25 @@ func searchPackage() {
 	must(dbe)
 	rgx := regexp.MustCompile(val)
 	fmt.Printf("Searching %#v...\n", val)
-	ids := []int{}
+	ids := new(IDArray)
 	for p, i := range *pn {
 		idx := rgx.FindStringIndex(p)
 		if idx != nil {
-			ids = append(ids, i)
+			ids.Add(i)
 		}
 	}
 	for i, dt := range *db {
 		idx := rgx.FindStringIndex(dt.Name)
 		if idx != nil {
-			ids = append(ids, i)
+			ids.Add(i)
 		}
 		idx = rgx.FindStringIndex(dt.Description)
 		if idx != nil {
-			ids = append(ids, i)
+			ids.Add(i)
 		}
 	}
-	for i := 0; i < len(ids); i++ {
-		data := (*db)[ids[i]]
+	for i := 0; i < ids.Size(); i++ {
+		data := (*db)[ids.Get(i)]
 		fmt.Printf("\n%s [%s] – %s\n %s\n", data.Name, data.PkgName, pkgTypeName[data.Type], data.Description)
 	}
 }
