9a5821b78e1a60fc65c6757b71dde7007e6ee650
Szewek <shevkpl@gmail.com>
2016-07-15 00:48:18 +0200

    Wrong test

--- mcpmdb/mcpmdb_test.go
+++ mcpmdb/mcpmdb_test.go
@@ -3,8 +3,6 @@ package mcpmdb
 import "testing"
 
 func TestMCPMDatabase(t *testing.T) {
-	ndb := &mcpmdbImpl{r: map[string]*MCPMPackage{}, w: map[string]*MCPMPackage{}}
-	ndb.open("tmpdb")
-	ndb.GetPackage("tinkers-construct")
-	ndb.Close()
+	GetPackage("tinkers-construct")
+	Close()
 }
